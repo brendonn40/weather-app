cd22d35f8079ae3fae52da6e972f91be761a94e8
brendon <brendoncesar40@gmail.com>
2022-10-19 18:34:36 -0300

    working version first design

--- src/domHandler.js
+++ src/domHandler.js
@@ -1,4 +1,8 @@
 export function displayData(data, info) {
+  createCurrent(data, info);
+}
+
+function createCurrent(data, info) {
   const location = document.getElementById("location");
   const temp = document.getElementById("temp");
   const date = document.getElementById("date");
@@ -6,8 +10,37 @@ export function displayData(data, info) {
   const weather = document.getElementById("weather");
   let degrees = Math.round(data[0].temp);
   location.innerText = `${info.city}, ${info.country}`;
-  date.innerText = data[0].date;
+  date.innerText = data[0].date.toDateString();
   temp.innerText = `${degrees}°C`;
   weather.innerText = data[0].weather.description;
   image.src = `http://openweathermap.org/img/wn/${data[0].weather.icon}.png`;
+}
+export function createForecast(data) {
+  clear("forecast");
+  for (let i = 1; i < 7; i++) {
+    const forecast = document.getElementById("forecast");
+    let day = document.createElement("div");
+    let image = document.createElement("img");
+    image.src = `http://openweathermap.org/img/wn/${data[i].weather.icon}.png`;
+    let date = document.createTextNode(data[i].date.toDateString());
+    let max = document.createTextNode(data[i].tempMax + "|");
+    let min = document.createTextNode(data[i].tempMin);
+    day.append(
+      date,
+      document.createElement("br"),
+      image,
+      document.createElement("br"),
+      max,
+      min
+    );
+    day.classList.add("day");
+    forecast.appendChild(day);
+  }
+}
+
+function clear(elementName) {
+  const content = document.getElementById(elementName);
+  while (content.firstChild) {
+    content.removeChild(content.firstChild);
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import "./style.css";
 import { grabWeatherData, getCoordinates } from "./apiStuff.js";
 import search from "./images/search.svg";
-import { displayData } from "./domHandler.js";
+import background from "./images/background.jpg";
+import { createForecast, displayData } from "./domHandler.js";
 const searchImg = document.getElementById("search");
 searchImg.src = search;
 searchImg.addEventListener("click", function (e) {
   e.stopPropagation();
   e.preventDefault();
   middle();
-  // grabWeatherData(city);
-  // let data = grabWeatherData(city);
-  // data.then((res) => displayData(res));
   document.getElementById("city").value = "";
 });
 
@@ -18,6 +16,6 @@ async function middle() {
   const city = document.getElementById("city").value;
   const coord = await getCoordinates(city);
   const forecast = await grabWeatherData(coord);
-  console.log(forecast[0].weather.description);
   displayData(forecast, coord[2]);
+  createForecast(forecast);
 }
